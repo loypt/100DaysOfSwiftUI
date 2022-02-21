6639ccbcca381b2e96497ba37c628b23d92dbf79
Loyio Hex <i@loyio.me>
2022-02-21 09:44:25 +0800

    add first swiftUI project with state and navigationView (#10)

---
+++ Day016-Project1-Part-One/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Loyio Hex on 2/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day016-Project1-Part-One/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Loyio Hex on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
