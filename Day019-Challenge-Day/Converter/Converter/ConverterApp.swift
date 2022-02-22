80bd603ac9d5938838abb08a2ac538e9579378ad
Loyio <i@loyio.me>
2022-02-22 13:57:12 +0800

    add first challenge app Converter

---
+++ Day019-Challenge-Day/Converter/Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Loyio Hex on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
